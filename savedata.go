a008a79fe194c618e23cd827ed49b6329ce1c843
trwy7 <47907719+trwy7@users.noreply.github.com>
2026-04-02 21:52:51 -0600

    feat: save data i swear i am going to keep forgetting to commit

--- savedata.go
+++ savedata.go
@@ -1,25 +1,55 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"errors"
 	"os"
 	"path/filepath"
 )
 
-func load_data() ([]map[string]any, error) {
+func get_data_path(file string) (string, bool, error) {
 	// Get the base directory (.config)
 	baseConfDir, err := os.UserConfigDir()
 	if err != nil {
-		return nil, err
+		return "", false, err
 	}
 	// Get the app directory (.config/trwy-canvas-lms-tui)
 	appConfDir := filepath.Join(baseConfDir, "trwy-canvas-lms-tui")
 	// Make it if it does not exist
 	os.MkdirAll(appConfDir, 0755)
 	// Get the final path for the config
-	confPath := filepath.Join(appConfDir, "servers.json")
+	confPath := filepath.Join(appConfDir, file)
+	// check if it exists
+	_, err = os.Stat(confPath)
+	if err == nil {
+		return confPath, true, nil
+	} else if errors.Is(err, os.ErrNotExist) {
+		return confPath, false, nil
+	}
+	// stackoverflow calls this shrodingers file, idk if this will actually ever be called
+	return confPath, false, err
+}
+
+func load_data() ([]map[string]string, error) {
+	// Get the data path
+	confPath, exists, err := get_data_path("servers.json")
+	if err != nil {
+		return nil, err
+	}
 	// Load the data
-	fmt.Println(baseConfDir)
+	if !exists {
+		return []map[string]string{}, nil
+	}
+	// Open and read the file
+	content, err := os.ReadFile(confPath)
+	if err != nil {
+		return nil, err
+	}
+	var smap []map[string]string
+	err = json.Unmarshal(content, &smap)
+	if err != nil {
+		return nil, err
+	}
 	// Return the data
-	return [], nil
+	return smap, nil
 }
