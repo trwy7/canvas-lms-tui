c86a0c66379c4df310db04a04851e9b751f30f48
trwy7 <47907719+trwy7@users.noreply.github.com>
2026-04-03 19:13:45 -0600

    fix: move config dir out of function

--- src/saves.py
+++ src/saves.py
@@ -3,20 +3,18 @@ import json
 import platform
 from pathlib import Path
 
-def get_config_dir():
-    home = Path.home()
-    if platform.system() == "Windows":
-        config_dir = Path(os.getenv("APPDATA")) / "trwy_canvas-lms-tui"
-    elif platform.system() == "Darwin":
-        config_dir = home / "Library" / "Application Support" / "trwy_canvas-lms-tui"
-    else:
-        config_dir = Path(os.getenv("XDG_CONFIG_HOME", home / ".config")) / "trwy_canvas-lms-tui"
-        
-    # Create the directory if it doesn't exist
-    config_dir.mkdir(parents=True, exist_ok=True)
-    return config_dir
-
-CONFIG_DIR = get_config_dir()
+# Get the home directory
+home = Path.home()
+# Find the config dir we should be using
+if platform.system() == "Windows":
+    CONFIG_DIR = Path(os.getenv("APPDATA")) / "trwy_canvas-lms-tui"
+elif platform.system() == "Darwin":
+    CONFIG_DIR = home / "Library" / "Application Support" / "trwy_canvas-lms-tui"
+else:
+    CONFIG_DIR = Path(os.getenv("XDG_CONFIG_HOME", home / ".config")) / "trwy_canvas-lms-tui"
+    
+# Create the directory if it doesn't exist
+CONFIG_DIR.mkdir(parents=True, exist_ok=True)
 
 def save_data(file: str, data):
     """Saves json data to a file
