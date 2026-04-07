d5f38d3a3c3a947032a2b889b1b3b6176a6c72b9
trwy7 <47907719+trwy7@users.noreply.github.com>
2026-04-07 09:34:40 -0600

    feat: conversation replies

--- src/views/inbox.py
+++ src/views/inbox.py
@@ -3,6 +3,7 @@ from InquirerPy.base import Choice
 from datetime import datetime
 from babel.dates import format_datetime
 import utils
+import requests
 
 def main(server: dict):
     while True:
@@ -36,9 +37,10 @@ def view_message(server: dict, message: dict):
         utils.clear(server['name'], "Inbox", message['subject'])
         # Get message 
         messages = utils.get_endpoint("/api/v1/conversations/" + str(message['id']))['json']
-        participant_nmap = {u['id']: u['name'] for u in messages['participants']}
+        # Print messages
+        participant_nmap = {u['id']: u['name'] for u in messages['participants']} # Author names are not included in the message for some reason
         for rmessage in messages['messages']:
-            print(participant_nmap[rmessage['author_id']] + " at " + format_datetime(datetime.fromisoformat(message['created_at'])) + ":")
+            print(participant_nmap[rmessage['author_id']] + " at " + format_datetime(datetime.fromisoformat(rmessage['created_at'])) + ":")
             print(rmessage['body'] + "\n")
         # We cannot mark as read for individual messages for some reason, if someone finds a way, make an issue!
         # Show messages
@@ -57,3 +59,21 @@ def view_message(server: dict, message: dict):
                 break
             case "reply":
                 reply_to_msg(server, message)
+
+def reply_to_msg(server: dict, message: dict):
+    # Request message
+    cmt = inquirer.text(
+        message="Enter reply text:",
+        multiline=True,
+    ).execute()
+    # Confirm
+    conf = inquirer.confirm(
+        message="Are you sure you want to send this message"
+    ).execute()
+    if not conf:
+        return
+    # Add it!
+    t = requests.post(f"{server['url']}/api/v1/conversations/{message['id']}/add_message", params={"body": cmt}, headers={"Authorization": f"Bearer {server['token']}"}, timeout=20)
+    if t.status_code != 200:
+        input("Request may have failed: " + str(t.status_code))
+    utils.clear_request_cache()
