363ecb956c13d94231b787060c19835696493b81
trwy7 <47907719+trwy7@users.noreply.github.com>
2026-04-07 09:46:16 -0600

    misc: change setup wording

--- src/views/setup.py
+++ src/views/setup.py
@@ -6,9 +6,9 @@ def add_instance(servers: list[dict[str,str]]):
     utils.clear()
     url = inquirer.text(message="Enter your canvas base url (including https):").execute().rstrip("/")
     while (not url.startswith("http")) or requests.get(url + "/help_links", timeout=5).status_code != 200:
-        url = inquirer.text(message="That dosen't seem valid. Remember to include https and remove the trailing slash:").execute()
+        url = inquirer.text(message="That dosen't seem like valid canvas URL. Remember to include https and remove the trailing slash:").execute()
     utils.set_current_instance({"name": "setup", "url": url, "token": ""})
     token, token_test_resp = utils.request_token(url)
-    name = inquirer.text(message=f"Hello, {token_test_resp['json']['first_name']}! Enter a name for this configuration:").execute()
+    name = inquirer.text(message=f"Hello, {token_test_resp['json']['first_name']}! Enter a name for this instance:").execute()
     servers.append({"name": name, "url": url, "token": token})
 
